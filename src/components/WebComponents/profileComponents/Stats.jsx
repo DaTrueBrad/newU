e4c3b159225440ba5e8cc54737c4369330a528ed
Brady Bott <bottbrady@gmail.com>
2021-10-13 10:24:49 -0600

    testing if form bug is actual bug

--- src/components/WebComponents/profileComponents/Stats.jsx
+++ src/components/WebComponents/profileComponents/Stats.jsx
@@ -1,19 +1,54 @@
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import Spinner from '../../Spinner'
+import { useFormik } from 'formik'
+
+
 
 function Stats() {
   const [data, setData] = useState()
   const [hide, setHide] = useState(false)
+  const [benchDate, setBenchDate] = useState('')
+  const initialValues = {
+    bench_date: '',
+    bench_stat: 0,
+    squat_date: '',
+    squat_stat: 0,
+    deadlift_date: '',
+    deadlift_stat: ''
+  }
+
   const GetStats = async () => {
     let user = +localStorage.getItem("user")
     let res = await axios.get('/userStats', {params: {user: user}})
     setData(res.data[0][0])
     console.log(data)
+    initialValues.bench_date = getDate(res.data[0][0].bench_date)
+    initialValues.squat_date = getDate(res.data[0][0].squat_date)
+    initialValues.deadlift_date = getDate(res.data[0][0].deadlift_date)
+    initialValues.bench_stat = res.data[0][0].bench_stat
+    initialValues.squat_stat = res.data[0][0].squat_stat
+    initialValues.deadlift_stat = res.data[0][0].deadlift_stat
   }
   useEffect(() => {
     GetStats()
   }, [])
+  
+
+  const onSubmit = (values) => {
+    alert(JSON.stringify(values))
+  }
+
+  function getDate(date) {
+    const d = new Date(date)
+    return d.toISOString().split('T')[0]
+  }
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit
+  })
+
   const Display = () => {
     if(data === undefined) {
       return <Spinner />
@@ -26,6 +61,7 @@ function Stats() {
               <th>Weight</th>
               <th>Date</th>
             </tr>
+            
             <tr>
               <td>Bench Press</td>
               <td>{data.bench_stat ? data.bench_stat : "TBD"}</td>
@@ -42,6 +78,51 @@ function Stats() {
               <td>{data.deadlift_date}</td>
             </tr>
           </tbody>
+          <div className="stat-input-container" id={`${hide ? "" : "hide-workout"}`}>
+            <form onSubmit={formik.handleSubmit}>
+              <fieldset>
+                <legend>Bench Press</legend>
+                <input
+                  type="number"
+                  name="bench_stat"
+                  value={formik.values.bench_stat}
+                  onChange={formik.handleChange}/>
+                <input
+                  type="date"
+                  name="bench_date"
+                  value={formik.values.bench_date}
+                  onChange={formik.handleChange}/>
+              </fieldset>
+              <fieldset>
+                <legend>Squat</legend>
+                <input
+                  type="number"
+                  name="squat_stat"
+                  value={formik.values.squat_stat}
+                  onChange={formik.handleChange}/>
+                <input
+                  type="date"
+                  name="squat_date"
+                  value={formik.values.squat_date}
+                  onChange={formik.handleChange}/>
+              </fieldset>
+              <fieldset>
+                <legend>Deadlift</legend>
+                <input
+                  type="number"
+                  name="deadlift_stat"
+                  value={formik.values.deadlift_stat}
+                  onChange={formik.handleChange}/>
+                <input
+                  type="date"
+                  name="deadlift_date"
+                  value={formik.values.deadlift_date}
+                  onChange={formik.handleChange}/>
+              </fieldset>
+              <button type="submit">Save</button>
+            </form>
+            
+          </div>
           <button onClick={() => setHide(!hide)}>Update</button>
         </table>
         
